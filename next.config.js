afc0cb9352a3e9934630f1a2b71956ea3c782a1f
Rahul Dhir <me@rahuldhir.com>
2025-02-26 18:22:31 -0700

    Fix global css

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["a.tile.openstreetmap.org", "b.tile.openstreetmap.org", "c.tile.openstreetmap.org", 'hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  },
+}
+
+module.exports = nextConfig
+
--- v0-user-next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["a.tile.openstreetmap.org", "b.tile.openstreetmap.org", "c.tile.openstreetmap.org"],
-  },
-}
-
-module.exports = nextConfig
-
